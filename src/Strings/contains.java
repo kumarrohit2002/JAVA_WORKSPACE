8bc0f259c21dd8cc392ba5c3a2a76be44eec2df2
Rohit kumar <rohitranjanrrsingh@gmail.com>
2023-12-28 20:06:28 +0530

    java workspace

---
+++ src/Strings/contains.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class contains {
+    public static void main(String[] args) {
+        String str="rohit";
+        System.out.println(str.contains("r"));
+        System.out.println(str.contains("m"));
+    }
+}
