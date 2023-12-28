332d72176ea66d40ec63f79c60bfb23564e2d229
Rohit kumar <rohitranjanrrsingh@gmail.com>
2023-12-28 19:38:22 +0530

    java workspace

---
+++ src/Strings/firstStr.java
@@ -0,0 +1,9 @@
+package Strings;
+import java.util.*;
+public class firstStr {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(str);
+    }
+}
