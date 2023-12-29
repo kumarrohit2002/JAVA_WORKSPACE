e2b93ff11572b92bdb9be3d9e27411f57a656c83
Rohit kumar <rohitranjanrrsingh@gmail.com>
2023-12-29 11:32:18 +0530

    java workspace

---
+++ src/Strings/predefindfun.java
@@ -0,0 +1,18 @@
+package Strings;
+//import java.util.*;
+public class predefindfun {
+    public static void main(String[] args) {
+        String str="Rohit";
+        String gtr=" kumar";
+        System.out.println(str.indexOf('h'));
+        System.out.println(str.startsWith("ro"));
+        System.out.println(str.endsWith("it"));
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.concat(gtr));
+
+
+    }
+}
+
+// indexOf() , length() , contans() , compareTo() ,  startsWith() , endsWith()  , toLowerCase() , toUpperCase() , comcat()
---
+++ src/Strings/substr.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class substr {
+    public static void main(String[] args) {
+        String str="abcd";
+        System.out.println(str.substring(0,3));
+        System.out.println(str.substring(1));
+    }
+}
