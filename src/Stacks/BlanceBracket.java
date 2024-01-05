36a2cba0a25f29a05b4234fc373e4eef61725d2c
Rohit kumar <rohitranjanrrsingh@gmail.com>
2024-01-06 01:24:50 +0530

    java workspace

--- src/Stacks/BlanceBracket.java
+++ src/Stacks/BlanceBracket.java
@@ -4,13 +4,21 @@ import java.util.Stack;
 
 public class BlanceBracket {
 
+    public static boolean ismatch(char open,char close){
+        return (open=='(' && close==')') ||
+                (open=='{' && close=='}') ||
+                (open=='[' && close==']');
+    }
+
     public static void main(String[] args) {
         Stack<Character> st=new Stack<>();
-        String s=" ( )( ( ) )";
+        String s=" [{( )}( ( () )]";
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==' ') continue;
-            if(s.charAt(i)=='(') st.push(s.charAt(i));
-            if(s.charAt(i)==')') st.pop();
+            char ch=s.charAt(i);
+            if(ch==' ') continue;
+            if(ch=='(' || ch=='{' || ch=='[') st.push(s.charAt(i));
+//            if(!st.isEmpty() && st.peek()==ch) st.pop();   // this is give worng answer
+            if(!st.isEmpty() && ismatch(st.peek(),ch)) st.pop();
         }
         if(st.empty()){
             System.out.println("String is Balanced Bracket!!");
